430f023f82db09287516f3f343af82d7961e2e92
Dovydas Montvydas <dovism@gmail.com>
2024-07-26 15:48:47 +0300

    not finished yet

---
+++ KnyguKolekcija3uzd/Models/Science.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnyguKolekcija3uzd.Models
+{
+    internal class Science : Book
+    {
+        public string Topic { get; set; }
+
+        public Science(string topic, string title, string author, DateOnly year)
+        {
+            Topic = topic;
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+    }
+}
---
+++ KnyguKolekcija3uzd/Services/BookListService.cs
@@ -0,0 +1,19 @@
+﻿using KnyguKolekcija3uzd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnyguKolekcija3uzd.Services
+{
+    internal class BookListService : Book
+    {
+        public List<Book> AllBooks { get; set; }
+
+        public BookListService()
+        {
+            AllBooks = new List<Book>();
+        }
+    }
+}
